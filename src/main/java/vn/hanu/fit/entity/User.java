a0e333c70b06dcbcdad5f6aea65ce67e7a720e30
chiendq <chiendq.work@gmail.com>
2022-03-04 14:39:19 +0700

    remove validation annotation

--- src/main/java/vn/hanu/fit/entity/User.java
+++ src/main/java/vn/hanu/fit/entity/User.java
@@ -20,29 +20,21 @@ public class User {
     private Long id;
 
     @Column(name = "username")
-//    @NotNull
-//    @Size(min = 8, max = 255, message = "Username is required")
     private String username;
 
     @Column(name = "password")
-//    @NotNull
-//    @Size(min = 8, max = 255, message = "Password is required")
     private String password;
 
-//    @NotNull
     @Column(name = "fullname")
-//    @Size(min = 8, max = 255, message = "Fullname is required")
     private String fullname;
 
     @Column(name = "phone")
-//    @Size(min = 10, max = 11, message = "Username is required")
     private String phone;
 
     @Column(name = "dateofbirth")
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private Date dateofbirth;
 
-//    @NotNull( message = "Gender is required")
     @Column(name = "gender")
     private boolean gender;
 
@@ -50,7 +42,6 @@ public class User {
     private String role;
 
     @Column(name = "email")
-//    @Email(message = "Email is not valid")
     private String email;
 
 
