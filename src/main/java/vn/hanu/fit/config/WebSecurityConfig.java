7c95a437e92b9dbedff6297d72216a1f9fc7a371
chiendq <chiendq.work@gmail.com>
2022-03-05 14:15:10 +0700

    add log (bugging)

--- src/main/java/vn/hanu/fit/config/WebSecurityConfig.java
+++ src/main/java/vn/hanu/fit/config/WebSecurityConfig.java
@@ -1,17 +1,22 @@
 package vn.hanu.fit.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import vn.hanu.fit.controller.UserController;
 import vn.hanu.fit.repository.UserRepository;
 import vn.hanu.fit.service.UserService;
 
@@ -23,15 +28,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserService userService;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+
+
     @Bean
     public PasswordEncoder passwordEncoder() {
 //        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+
         return new BCryptPasswordEncoder();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth)
             throws Exception {
+        LOGGER.info("WEBSECURITYCONFIG");
         auth.userDetailsService(userService); // Provide userservice for spring security
 //                .passwordEncoder(passwordEncoder()); // Provide password encoder
     }
@@ -43,13 +53,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                 .antMatchers("/").permitAll() // Allow every one access to this address
-                .anyRequest().authenticated() // All other request must be authenticated
-                .and()
-                .formLogin() // Allow user authenticate with form login
-                .defaultSuccessUrl("/admin")
-                .permitAll() // Tất cả đều được truy cập vào địa chỉ này
-                .and()
-                .logout() // Allow logout
-                .permitAll();
+                .antMatchers("/join").permitAll() // Allow every one access to this address
+                .antMatchers("/login").permitAll() // Allow every one access to this address
+                .anyRequest().authenticated(); // All other request must be authenticated
+//                .and();
+//
+//                .permitAll() // Tất cả đều được truy cập vào địa chỉ này
+//                .and()
+//                .logout() // Allow logout
+//                .permitAll();
     }
 }
