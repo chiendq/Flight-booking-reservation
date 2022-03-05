20fc357815070e478f985afa1adbdd0908da990c
chiendq <chiendq.work@gmail.com>
2022-03-05 12:59:13 +0700

    remove validation

--- src/main/java/vn/hanu/fit/dto/LoginDTO.java
+++ src/main/java/vn/hanu/fit/dto/LoginDTO.java
@@ -5,18 +5,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
-
-
 @Getter
 @Setter
 @NoArgsConstructor
 public class LoginDTO {
-    @Min(value = 8, message = "Username must at least 8 characters ")
     private String username;
 
-    @Min(value = 8, message = "Password must at least 8 characters ")
     private String password;
 
     private boolean status = false;
