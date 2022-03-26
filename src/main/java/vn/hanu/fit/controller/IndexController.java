666fe5bd3dd129cba02480fbe07fbccd80921974
tuthoeng123 <70209314+tuthoeng123@users.noreply.github.com>
2022-03-26 13:20:14 +0700

    iwjdaf

--- src/main/java/vn/hanu/fit/controller/IndexController.java
+++ src/main/java/vn/hanu/fit/controller/IndexController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -43,6 +44,21 @@ public class IndexController {
         model.addAttribute("ticketSearchDTO", new TicketSearchDTO());
         model.addAttribute("flightClass", flightClassList);
         return "index";
+    }
+
+    @RequestMapping("/seat")
+    public String seat(Model model){
+        model.addAttribute("airports", airportRepository.findAll());
+        model.addAttribute("ticketSearchDTO", new TicketSearchDTO());
+        model.addAttribute("flightClass", flightClassList);
+        return "seat";
+    }
+    @RequestMapping("/booking")
+    public String book(Model model){
+        model.addAttribute("airports", airportRepository.findAll());
+        model.addAttribute("ticketSearchDTO", new TicketSearchDTO());
+        model.addAttribute("flightClass", flightClassList);
+        return "booking";
     }
 
     @RequestMapping("/search")
