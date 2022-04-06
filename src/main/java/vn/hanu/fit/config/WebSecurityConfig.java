eafc63389b4dbaf5f77e88027a4cd82c68c8e092
chiendq <chiendq.work@gmail.com>
2022-04-06 15:06:43 +0700

    change sql query to fit with req, for WebSecurityConfig.java , i added in heroku database explorer so i temp comment that class

--- src/main/java/vn/hanu/fit/config/WebSecurityConfig.java
+++ src/main/java/vn/hanu/fit/config/WebSecurityConfig.java
@@ -1,69 +1,69 @@
-package vn.hanu.fit.config;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import vn.hanu.fit.controller.AuthenticationController;
-import vn.hanu.fit.service.UserDetailsServiceImpl;
-
-
-@Configuration
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(AuthenticationController.class);
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        return new UserDetailsServiceImpl();
-    }
-
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailsService());
-        authProvider.setPasswordEncoder(passwordEncoder());
-        return authProvider;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(new BCryptPasswordEncoder().encode("12345678"));
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authenticationProvider());
-    }
-
-    // Override this method and left empty to disable default setting of Spring Boot Security
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
-        http.cors().and().csrf().disable();
-                http
-            .authorizeRequests()
-            .anyRequest().permitAll()
-            .and()
-            .formLogin()// Cho phép người dùng xác thực bằng form login
-            .loginPage("/login")
-            .failureUrl("/login?error")
-            .permitAll() // Tất cả đều được truy cập vào địa chỉ này
-            .and()
-            .logout() // Cho phép logout
-            .permitAll();
-    }
-}
+//package vn.hanu.fit.config;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import vn.hanu.fit.controller.AuthenticationController;
+//import vn.hanu.fit.service.UserDetailsServiceImpl;
+//
+//
+//@Configuration
+//@EnableWebSecurity
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    private static final Logger LOGGER = LoggerFactory.getLogger(AuthenticationController.class);
+//
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        return new UserDetailsServiceImpl();
+//    }
+//
+//    @Bean
+//    public BCryptPasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Bean
+//    public DaoAuthenticationProvider authenticationProvider() {
+//        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+//        authProvider.setUserDetailsService(userDetailsService());
+//        authProvider.setPasswordEncoder(passwordEncoder());
+//        return authProvider;
+//    }
+//
+//    public static void main(String[] args) {
+//        System.out.println(new BCryptPasswordEncoder().encode("12345678"));
+//    }
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.authenticationProvider(authenticationProvider());
+//    }
+//
+//    // Override this method and left empty to disable default setting of Spring Boot Security
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//
+//        http.cors().and().csrf().disable();
+//                http
+//            .authorizeRequests()
+//            .anyRequest().permitAll()
+//            .and()
+//            .formLogin()// Cho phép người dùng xác thực bằng form login
+//            .loginPage("/login")
+//            .failureUrl("/login?error")
+//            .permitAll() // Tất cả đều được truy cập vào địa chỉ này
+//            .and()
+//            .logout() // Cho phép logout
+//            .permitAll();
+//    }
+//}
