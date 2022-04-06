ea5e42b8f2ab2ca8c911bf9c43fbabaf26b4839a
chiendq <chiendq.work@gmail.com>
2022-04-06 15:09:36 +0700

    misstake with WebSecurityConfig.java -> InitializeData.java

--- src/main/java/vn/hanu/fit/repository/InitializeData.java
+++ src/main/java/vn/hanu/fit/repository/InitializeData.java
@@ -1,23 +1,23 @@
-package vn.hanu.fit.repository;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
-import org.springframework.stereotype.Component;
-
-import javax.sql.DataSource;
-
-@Component
-public class InitializeData {
-
-    @Autowired
-    private DataSource dataSource;
-
-    @EventListener(ApplicationReadyEvent.class)
-    public void loadData() {
-        ResourceDatabasePopulator resourceDatabasePopulator = new ResourceDatabasePopulator(false, false, "UTF-8", new ClassPathResource("static/initdb_1.0.sql"));
-        resourceDatabasePopulator.execute(dataSource);
-    }
-}
+//package vn.hanu.fit.repository;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.context.event.ApplicationReadyEvent;
+//import org.springframework.context.event.EventListener;
+//import org.springframework.core.io.ClassPathResource;
+//import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+//import org.springframework.stereotype.Component;
+//
+//import javax.sql.DataSource;
+//
+//@Component
+//public class InitializeData {
+//
+//    @Autowired
+//    private DataSource dataSource;
+//
+//    @EventListener(ApplicationReadyEvent.class)
+//    public void loadData() {
+//        ResourceDatabasePopulator resourceDatabasePopulator = new ResourceDatabasePopulator(false, false, "UTF-8", new ClassPathResource("static/initdb_1.0.sql"));
+//        resourceDatabasePopulator.execute(dataSource);
+//    }
+//}
