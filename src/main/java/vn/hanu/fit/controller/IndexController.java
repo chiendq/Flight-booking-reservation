2092b55373aa42b37c9e5764484751002896ce8a
tuthoeng123 <70209314+tuthoeng123@users.noreply.github.com>
2022-05-04 20:03:46 +0700

    new

--- src/main/java/vn/hanu/fit/controller/IndexController.java
+++ src/main/java/vn/hanu/fit/controller/IndexController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -43,6 +44,10 @@ public class IndexController {
         return "index";
     }
 
+    @GetMapping("/comingsoon")
+    public String comingsoon(){
+        return "comingsoon";
+    }
     @RequestMapping("/seat")
     public String seat(Model model) {
         model.addAttribute("airports", airportRepository.findAll());
