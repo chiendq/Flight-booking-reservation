220c12dffe5d0613807d9cb66ea09d8b2dbd42b5
chiendq <chiendq@gmail.com>
2022-05-04 13:02:12 +0700

    updating edit user profille

--- src/main/java/vn/hanu/fit/controller/UserProfile.java
+++ src/main/java/vn/hanu/fit/controller/UserProfile.java
@@ -1,25 +1,53 @@
 package vn.hanu.fit.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import vn.hanu.fit.dto.EditUserInfoDto;
 import vn.hanu.fit.entity.User;
 import vn.hanu.fit.repository.UserRepository;
+import vn.hanu.fit.userdetails.MyUserDetails;
+
+import java.util.Date;
+
 @Controller
+@Slf4j
 public class UserProfile {
-    @Autowired
     UserRepository userRepository;
 
-    @Autowired
     BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public UserProfile(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
 
     @RequestMapping("/manage")
     public String showSignup(Model model) {
-        return "profile";
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            String currentUserName = authentication.getName();
+            MyUserDetails userDetails = (MyUserDetails) authentication.getPrincipal();
+            EditUserInfoDto user = EditUserInfoDto.builder()
+                    .fullName(userDetails.getFullName())
+                    .password("*********")
+                    .email(userDetails.getEmail())
+                    .identityNumber(userDetails.getIdentityNumber())
+                    .phoneNumber(userDetails.getPhoneNumber())
+                    .gender(userDetails.getGender())
+                    .dateOfBirth(userDetails.getDateOfBirth())
+                    .build();
+            model.addAttribute("user", user);
+            return "profile";
+        }
+        return "redirect:/";
     }
 }
